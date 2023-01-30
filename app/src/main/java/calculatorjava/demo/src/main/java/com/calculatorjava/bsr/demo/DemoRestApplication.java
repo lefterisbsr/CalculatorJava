c04c29ecb12090cdc5907e6ab6086f0189bed636
devpro-cmd <123990071+devpro-cmd@users.noreply.github.com>
2023-01-30 21:34:45 +0100

    build project

---
+++ app/src/main/java/calculatorjava/demo/src/main/java/com/calculatorjava/bsr/demo/DemoRestApplication.java
@@ -0,0 +1,11 @@
+package com.calculatorjava.bsr.demo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoRestApplication extends Application {
+}
